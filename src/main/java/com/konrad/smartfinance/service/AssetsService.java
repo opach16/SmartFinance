fc9c13c01f131ef934d37cfb1c3ebcdb65fd9228
Konrad Opach <opach16@outlook.com>
2025-01-10 22:47:32 +0100

    refactoring

--- src/main/java/com/konrad/smartfinance/service/AssetsService.java
+++ src/main/java/com/konrad/smartfinance/service/AssetsService.java
@@ -5,7 +5,6 @@ import com.konrad.smartfinance.repository.AssetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -18,24 +17,21 @@ public class AssetsService {
         return assetRepository.findAll();
     }
 
-    public Asset addAsset(Asset asset) {
-        Asset fetchedAsset;
-        if (assetRepository.findByName(asset.getName()).isPresent()) {
-            fetchedAsset = assetRepository.findByName(asset.getName()).get();
-            fetchedAsset.setAmount(fetchedAsset.getAmount().add(asset.getAmount()));
-            assetRepository.save(fetchedAsset);
-        } else {
-            fetchedAsset = assetRepository.save(asset);
-        }
-        return fetchedAsset;
+    public void addAsset(Asset asset) {
+        assetRepository.findByName(asset.getName()).ifPresentOrElse(presentAsset -> {
+            presentAsset.setAmount(presentAsset.getAmount().add(asset.getAmount()));
+           assetRepository.save(presentAsset);
+           }, () -> assetRepository.save(asset));
     }
 
     public void deleteAsset(Asset asset) {
-        Asset fetchedAsset = assetRepository.findByName(asset.getName()).get();
-        fetchedAsset.setAmount(fetchedAsset.getAmount().subtract(asset.getAmount()));
-        assetRepository.save(fetchedAsset);
-        if (fetchedAsset.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
-            assetRepository.delete(fetchedAsset);
-        }
+        assetRepository.findByName(asset.getName()).ifPresent(presentAsset -> {
+            if (presentAsset.getAmount().compareTo(asset.getAmount()) <= 0) {
+                assetRepository.delete(presentAsset);
+            } else {
+                presentAsset.setAmount(presentAsset.getAmount().subtract(asset.getAmount()));
+                assetRepository.save(presentAsset);
+            }
+        });
     }
 }
